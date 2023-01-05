f57a4febaaa54a8a06500bbf004594aa73dcbf34
Oscar Dobson-Brown <oscardobsonbrown@outlook.com>
2023-01-05 19:02:32 +0800

    Completed 2.7 - Check if Name Exists

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,34 @@
 import { useState } from "react";
-import Persons from "./components/Persons.js";
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-	const [newName, setNewName] = useState("a new person...");
+	const [persons, setPersons] = useState([{ name: "Arto Hellas", key: 1 }]);
+	const [newName, setNewName] = useState("");
 
-	// const Persons = ({ person }) => {
-	// 	return <li>{person.content}</li>;
-	// };
+	function isNewName(name) {
+		return name.name === newName;
+	}
 
-	const addPerson = (event) => {
+	const checkPersons = (event) => {
 		event.preventDefault();
-		console.log("button clicked", event.target);
-		const personObject = {
-			content: newName,
-			date: new Date().toISOString(),
-			id: persons.length + 1,
-		};
+		console.log(persons);
 
+		if (persons.find(isNewName)) {
+			alert(`${newName} is already added to the phonebook`);
+			setNewName("");
+		} else {
+			addPersons();
+			console.log(newName, "added");
+		}
+	};
+
+	const addPersons = (event) => {
+		const personObject = {
+			name: newName,
+			key: persons.length + 1,
+		};
 		setPersons(persons.concat(personObject));
 		setNewName("");
+		// console.log("button clicked", event.target);
 	};
 
 	const handleNameChange = (event) => {
@@ -27,26 +36,20 @@ const App = () => {
 		setNewName(event.target.value);
 	};
 
-	<div>debug: {newName}</div>;
-
 	return (
 		<div>
+			<div>debug: {newName}</div>
 			<h2>Phonebook</h2>
-			{/* <form>
+			<form onSubmit={checkPersons}>
 				<div>
-					name: <input />
-				</div>
-				<div>
+					<label>Name: </label>
+					<input value={newName} onChange={handleNameChange} />
 					<button type="submit">add</button>
 				</div>
-			</form> */}
-			<form onSubmit={addPerson}>
-				<input value={newName} onChange={handleNameChange} />
-				<button type="submit">save</button>
 			</form>
 			<h2>Numbers</h2>
 			{persons.map((person) => (
-				<Persons key={person.id} person={person} />
+				<p key="person.key">{person.name}</p>
 			))}
 		</div>
 	);
--- part2/phonebook/src/components/Persons.js
+++
@@ -1,5 +0,0 @@
-const Persons = ({ person }) => {
-	return <p key={person.id}>{person.content}</p>;
-};
-
-export default Persons;
